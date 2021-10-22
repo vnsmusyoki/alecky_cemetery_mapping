c16113b1933f7dc3f15f65bf25389411a72cc8bf
unknown <vnsmusyoki@gmail.com>
2021-10-22 13:45:27 +0300

    started project

---
+++ README.md
@@ -0,0 +1,18 @@
+
+## About Roodito
+
+It is a school management weiste for teachers, students, parents and tutors
+
+## About Languages
+-HTML
+-CSS
+-Bootstrap
+-Javascript
+-Laravel
+-Livewire
+-Jquery
+-Ajax
+-MySQL
+
+
+ 
---
+++ app/Models/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Laratrust\Traits\LaratrustUserTrait;
+
+class User extends Authenticatable implements MustVerifyEmail
+{
+    use LaratrustUserTrait;
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var string[]
+     */
+
+ 
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'slag'
+
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
