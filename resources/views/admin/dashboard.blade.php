b0700918c16c60c9489dff701557d1363228b675
unknown <vnsmusyoki@gmail.com>
2021-11-28 17:56:44 +0300

    layout admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,65 @@
+@extends('admin.layout')
+@section('content')
+<div class="row">
+    <div class="col-xl-3 col-sm-6 col-12 d-flex">
+        <div class="card bg-one w-100">
+            <div class="card-body">
+                <div class="db-widgets d-flex justify-content-between align-items-center">
+                    <div class="db-icon">
+                        <i class="fas fa-user-graduate"></i>
+                    </div>
+                    <div class="db-info">
+                        <h3>50055</h3>
+                        <h6>Students</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-sm-6 col-12 d-flex">
+        <div class="card bg-two w-100">
+            <div class="card-body">
+                <div class="db-widgets d-flex justify-content-between align-items-center">
+                    <div class="db-icon">
+                        <i class="fas fa-crown"></i>
+                    </div>
+                    <div class="db-info">
+                        <h3>50+</h3>
+                        <h6>Awards</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-sm-6 col-12 d-flex">
+        <div class="card bg-three w-100">
+            <div class="card-body">
+                <div class="db-widgets d-flex justify-content-between align-items-center">
+                    <div class="db-icon">
+                        <i class="fas fa-building"></i>
+                    </div>
+                    <div class="db-info">
+                        <h3>30+</h3>
+                        <h6>Department</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-xl-3 col-sm-6 col-12 d-flex">
+        <div class="card bg-four w-100">
+            <div class="card-body">
+                <div class="db-widgets d-flex justify-content-between align-items-center">
+                    <div class="db-icon">
+                        <i class="fas fa-file-invoice-dollar"></i>
+                    </div>
+                    <div class="db-info">
+                        <h3>$505</h3>
+                        <h6>Revenue</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
